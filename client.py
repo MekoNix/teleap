679a96500144c33f530f8ee5f0aea7ae1fa0f779
MekoMix <p.ryabukhin@gmail.com>
2023-01-26 21:12:14 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,4 +1,12 @@
 import socket, threading
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_lenght = len(message)
+    send_length = str(msg_lenght).encode(FORMAT)
+    send_length+= b' '* (HEADER-len(send_length))
+    client.send(send_length)
+    client.send(message)
+
 HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
@@ -8,12 +16,6 @@ ADDR = (SERVER, PORT)
 
 client = socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect()
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_lenght = len(message)
-    send_length = str(msg_lenght).encode(FORMAT)
-    send_length+= b' '* (HEADER-len(send_length))
-    client.send(send_length)
-    client.send(message)
+
 send("Test")
 send(DISCONNECT_MESSAGE)
